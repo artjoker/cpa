2fe7249c49cf7730cbfd16b4ad31a902ebc8bf7b
user <vt2company@gmail.com>
2020-06-12 10:45:17 +0300

    Changed the StormDigital Provider

--- src/Providers/StormDigital/Lead/Parser.php
+++ src/Providers/StormDigital/Lead/Parser.php
@@ -10,15 +10,17 @@ class Parser implements \Artjoker\Cpa\Interfaces\Lead\LeadParser
 {
     use QueryParams;
 
-    protected const UTM_SOURCE = 'stormdigital';
-    protected const AFF_SUB    = 'aff_sub';
-    protected const AFF_ID     = 'aff_id';
+    protected const UTM_SOURCES = [
+        'stormdigital',
+        'storm',
+    ];
+    protected const CLICK_ID   = 'transaction_id';
 
     public function parse(string $url): ?LeadInfo
     {
         $query = $this->getQueryParams($url);
-        $isQueryValid = ($query['utm_source'] ?? null) === static::UTM_SOURCE
-            && array_key_exists(static::AFF_SUB, $query);
+        $isQueryValid = in_array($query['utm_source'] ?? null, static::UTM_SOURCES, true)
+            && array_key_exists(static::CLICK_ID, $query);
 
         if (!$isQueryValid) {
             return null;
@@ -27,8 +29,7 @@ class Parser implements \Artjoker\Cpa\Interfaces\Lead\LeadParser
         return new LeadInfo(
             \Artjoker\Cpa\Interfaces\Lead\LeadSource::STORM_DIGITAL,
             [
-                'clickId' => $query[static::AFF_SUB],
-                'pid'     => $query[static::AFF_ID] ?? null,
+                'clickId' => $query[static::CLICK_ID]
             ]
         );
     }
