ce784c5eaacff8b4e50a8824f7edd5a94f76f4cd
user <vt2company@gmail.com>
2021-03-03 18:33:51 +0200

    Fixed bug

--- src/Conversion/ConversionService.php
+++ src/Conversion/ConversionService.php
@@ -1,96 +1,99 @@
 <?php
 
-namespace Artjoker\Cpa\Conversion;
+    namespace Artjoker\Cpa\Conversion;
 
-use Artjoker\Cpa\Interfaces\Conversion\ServiceInterface;
-use Artjoker\Cpa\Lead\LeadService;
-use Artjoker\Cpa\Models\Conversion;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Str;
+    use Artjoker\Cpa\Interfaces\Conversion\ServiceInterface;
+    use Artjoker\Cpa\Lead\LeadService;
+    use Artjoker\Cpa\Models\Conversion;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Support\Facades\Config;
+    use Illuminate\Support\Facades\Log;
+    use Illuminate\Support\Str;
 
-class ConversionService implements ServiceInterface
-{
-    public $senders;
-    /**
-     * @var LeadService
-     */
-    protected $leadService;
-
-    /**
-     * ConversionService constructor.
-     * @param  LeadService  $leadService
-     */
-    public function __construct(LeadService $leadService)
+    class ConversionService implements ServiceInterface
     {
-        $this->leadService = $leadService;
-    }
+        public $senders;
+        /**
+         * @var LeadService
+         */
+        protected $leadService;
 
-
-    /**
-     * Register conversion when goal is achieved
-     * e.g. register 'sale' or 'register' event
-     *
-     * @param Model|int|string $leadModel user who performs target action
-     * @param string $conversionId internal conversion Id, for example orderId or userId
-     * @param string $event registered event (declared in config)
-     * @return Conversion|null
-     */
-    public function register($leadModel, string $conversionId, string $event): ?Conversion
-    {
-        if (empty($event) || !Config::has('cpa.events.' . $event)) {
-            Log::debug("Trying to send conversion {$conversionId} with undefined 'event'", [static::class, $event]);
-            return null;
-        }
-        $lead = $this->leadService->getLastLeadByUser($leadModel);
-        if (is_null($lead)) {
-            return null;
-        }
-        $source = $lead->source;
-        $sender = (new SendServiceFactory($source, $event))->create();
-        if (is_null($sender)) {
-            return null;
+        /**
+         * ConversionService constructor.
+         *
+         * @param LeadService $leadService
+         */
+        public function __construct(LeadService $leadService)
+        {
+            $this->leadService = $leadService;
         }
 
-        $conversion = new Conversion();
-        $conversion->lead()->associate($lead);
-        $conversion->conversion_id = $conversionId;
-        $conversion->event = $event;
 
-        if ($conversion->isExists()) {
-            Log::info("Skipping sending duplicate conversion $conversionId", [static::class, $source]);
-            return null;
-        }
+        /**
+         * Register conversion when goal is achieved
+         * e.g. register 'sale' or 'register' event
+         *
+         * @param Model|int|string $leadModel    user who performs target action
+         * @param string           $conversionId internal conversion Id, for example orderId or userId
+         * @param string           $event        registered event (declared in config)
+         *
+         * @return Conversion|null
+         */
+        public function register($leadModel, string $conversionId, string $event): ?Conversion
+        {
+            if (empty($event) || !Config::has('cpa.events.' . $event)) {
+                Log::debug("Trying to send conversion {$conversionId} with undefined 'event'", [static::class, $event]);
+                return null;
+            }
+            $lead = $this->leadService->getLastLeadByUser($leadModel);
+            if (is_null($lead)) {
+                return null;
+            }
+            $source = $lead->source;
+            $sender = (new SendServiceFactory($source, $event))->create();
+            if (is_null($sender)) {
+                return null;
+            }
 
-        /** @var Postback $result */
-        $result = $sender->send($conversion, $this->getEventParams($event, $source));
-        $conversion->request = [
-            'method' => $result->getRequest()->getMethod(),
-            'uri' => (string)$result->getRequest()->getUri(),
-            'body' => $result->getRequest()->getBody()->getContents(),
-        ];
+            $conversion = new Conversion();
+            $conversion->lead()->associate($lead);
+            $conversion->conversion_id = $conversionId;
+            $conversion->event         = $event;
 
-        $response = $result->getResponse();
+            if ($conversion->isExists()) {
+                Log::info("Skipping sending duplicate conversion $conversionId", [static::class, $source]);
+                return null;
+            }
 
-        if ($response !== null && $response->getBody() && $response->getBody()->getContentLength() > 0) {
-            $conversion->response = [
-                'code' => $response->getStatusCode(),
-                'body' => $response->getBody()->getContents(),
+            /** @var Postback $result */
+            $result              = $sender->send($conversion, $this->getEventParams($event, $source));
+            $conversion->request = [
+                'method' => $result->getRequest()->getMethod(),
+                'uri'    => (string)$result->getRequest()->getUri(),
+                'body'   => $result->getRequest()->getBody()->getContents(),
             ];
-        } else {
-            Log::error("Response for conversion $conversionId does not formed well.", [static::class, $response]);
+
+            $response = $result->getResponse();
+
+            $body = $response->getBody()->getContents();
+            if ($response !== null && is_string($body) && is_array(json_decode($body, true))) {
+                $conversion->response = [
+                    'code' => $response->getStatusCode(),
+                    'body' => $response->getBody()->getContents(),
+                ];
+            } else {
+                Log::error("Response for conversion $conversionId does not formed well.", [static::class, $response]);
+            }
+            $conversion->save();
+
+            return $conversion;
         }
-        $conversion->save();
 
-        return $conversion;
+        private function getEventParams($event, $source, $default = []): array
+        {
+            $source = Str::snake($source);
+            $params = Config::get('cpa.events.' . $event . '.' . $source, $default);
+
+            return (array)$params;
+        }
     }
-
-    private function getEventParams($event, $source, $default = []): array
-    {
-        $source = Str::snake($source);
-        $params = Config::get('cpa.events.' . $event . '.' . $source, $default);
-
-        return (array) $params;
-    }
-}
