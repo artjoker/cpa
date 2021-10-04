27a6e9e2281e8bd6375e6911294185c2035af964
user <vt2company@gmail.com>
2021-10-04 13:02:43 +0300

    Added the new CPA provider MoneyGo

---
+++ src/Providers/MoneyGo/Lead/Parser.php
@@ -0,0 +1,33 @@
+<?php
+
+    namespace Artjoker\Cpa\Providers\MoneyGo\Lead;
+
+    use Artjoker\Cpa\Lead\LeadInfo;
+    use Artjoker\Cpa\Traits\QueryParams;
+
+    class Parser implements \Artjoker\Cpa\Interfaces\Lead\LeadParser
+    {
+        use QueryParams;
+
+        protected const UTM_SOURCE = 'moneygo';
+        protected const CLICK_ID   = 'clickid';
+
+        public function parse(string $url): ?LeadInfo
+        {
+            $query        = $this->getQueryParams($url);
+            $isQueryValid = ($query['utm_source'] ?? null) === static::UTM_SOURCE
+                && array_key_exists(static::CLICK_ID, $query);
+
+            if (!$isQueryValid) {
+                return null;
+            }
+
+            return new LeadInfo(
+                \Artjoker\Cpa\Interfaces\Lead\LeadSource::MONEY_GO,
+                [
+                    'click_id'   => $query[static::CLICK_ID],
+                    'utm_medium' => $query['utm_medium'] ?? null,
+                ]
+            );
+        }
+    }
